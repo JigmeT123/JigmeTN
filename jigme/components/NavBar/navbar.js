1be280ac041edaa7a632061d03030f81c89fe504
jigmet12 <jigmetashi02@gmail.com>
2020-10-27 22:17:25 +0400

    next starter app

---
+++ jigme/components/NavBar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './navbar.module.css';
+
+const navbar = () => {
+    return (
+        <div className={styles.navBarContainer}>
+            <div className={styles.navLogo}>
+                
+            </div>
+            <div className={styles.ham}></div>
+        </div>
+    )
+}
+
+export default navbar
