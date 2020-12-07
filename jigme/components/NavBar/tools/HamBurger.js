ebd473d020856e970560891c0a91197affb4aa39
jigmet12 <jigmetashi02@gmail.com>
2020-12-07 22:56:22 +0400

    FIXED: menu

--- jigme/components/NavBar/tools/HamBurger.js
+++ jigme/components/NavBar/tools/HamBurger.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import styles from './ham.module.css';
+import {HiMenuAlt1} from 'react-icons/hi';
 
 const HamBurger = ({click}) => {
     return (
-        <button className={styles.hamburger} onClick={click}>
-            <div className={styles.hamLine}></div>
-            <div className={styles.hamLine}></div>
-            <div className={styles.hamLine}></div>
-        </button>
+        <HiMenuAlt1 className={styles.hamburger} onClick={click}/>
     )
 }
 
